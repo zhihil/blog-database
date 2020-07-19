ebdfa98dee0aa4b7a1845ea78614fbfe172add32
Tathmesh <tathmesh@github.com>
2020-07-19 16:55:43 -0400

    feat: added working database migrations

---
+++ knexfile.ts
@@ -0,0 +1,14 @@
+module.exports = {
+
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      port: "3100",
+      user: "postgres",
+      password: "docker",
+      database: "postgres"
+    }
+  },
+
+};
